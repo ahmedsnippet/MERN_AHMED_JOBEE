c2fdaf5588c17c2090e75f42d070bff1fdbaa0ef
ahmedsnippet <ahmedsnippet@gmail.com>
2024-12-06 22:24:59 -0800

    ChagesBack

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ import applicationRouter from "./Routers/applicationRouter.js"
 const app = express()
 
 app.use(cors({
-    origin: [process.env.FRONT_END],
+    origin: 'http://localhost:5173',
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
